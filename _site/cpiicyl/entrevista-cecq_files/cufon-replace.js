8ca7904774eeaac50ab82b812aaff9198789c012
cecuesta <cecuesta@gmail.com>
2015-06-28 20:52:07 +0200

    jekyll

---
+++ _site/cpiicyl/entrevista-cecq_files/cufon-replace.js
@@ -0,0 +1,2 @@
+Cufon.replace('.title h3, h1, h2, #custom #block-block-3 span, #right-col #block-block-3 span', { fontFamily:'Amerigo BT' , hover: true });
+Cufon.replace('#block-block-19 li span', { fontFamily:'Myriad Pro' , hover: true });
